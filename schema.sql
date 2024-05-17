bfa3bb96051fa0c03da335e3fa9203fdc959ad09
Janne Salokoski <salokoski.janne@gmail.com>
2024-05-17 06:20:49 +0300

    Add schema.sql to initialize the database

---
+++ schema.sql
@@ -0,0 +1,99 @@
+DROP TABLE IF EXISTS collections 	CASCADE;
+DROP TABLE IF EXISTS resources 		CASCADE;
+DROP TABLE IF EXISTS access_groups 	CASCADE;
+DROP TABLE IF EXISTS users 			CASCADE;
+DROP TABLE IF EXISTS bookings 		CASCADE;
+
+CREATE TABLE collections (
+	serial_id 	SERIAL PRIMARY KEY,
+	uuid_id 	UUID UNIQUE DEFAULT gen_random_uuid(),
+
+	name 		TEXT NOT NULL
+);
+
+CREATE TABLE resources (
+	serial_id 	SERIAL PRIMARY KEY,
+	uuid_id 	UUID UNIQUE DEFAULT gen_random_uuid(),
+
+	name 		TEXT NOT NULL,
+
+	collection_id INT NOT NULL REFERENCES collections(serial_id)
+);
+
+CREATE TABLE access_groups (
+	serial_id 	SERIAL PRIMARY KEY,
+	uuid_id 	UUID UNIQUE NOT NULL DEFAULT gen_random_uuid(),
+
+	name 		TEXT NOT NULL
+);
+
+CREATE TABLE users (
+	serial_id 	SERIAL PRIMARY KEY,
+	uuid_id 	UUID UNIQUE DEFAULT gen_random_uuid(),
+
+	name 		TEXT NOT NULL,
+	password 	TEXT NOT NULL,
+	
+	access_group_id INT NOT NULL REFERENCES access_groups(serial_id)
+);
+
+CREATE TABLE bookings (
+	serial_id 	SERIAL PRIMARY KEY,
+	uuid_id 	UUID UNIQUE DEFAULT gen_random_uuid(),
+
+	title 		TEXT NOT NULL,
+	description TEXT,
+	start_time 	TIMESTAMP NOT NULL,
+	end_time 	TIMESTAMP NOT NULL,
+
+	user_id 	INT NOT NULL REFERENCES users(serial_id),
+	resource_id INT NOT NULL REFERENCES resources(serial_id)
+);
+
+-- mock data
+
+INSERT INTO collections (name)
+VALUES
+	  ('Uusi ylioppilastalo')
+	, ('Domus Gaudium')
+	;
+
+INSERT INTO resources (name, collection_id)
+VALUES
+	  ('Alina', 		(SELECT serial_id FROM collections WHERE name='Uusi ylioppilastalo'))
+	, ('Wilhelmsson', 	(SELECT serial_id FROM collections WHERE name='Uusi ylioppilastalo'))
+	, ('Groudon', 		(SELECT serial_id FROM collections WHERE name='Domus Gaudium'))
+	;
+
+INSERT INTO access_groups (name)
+VALUES
+	  ('Admin')
+	, ('User')
+	, ('Guest')
+	;
+
+INSERT INTO users (name, password, access_group_id)
+VALUES
+	  ('Admin', 'password', (SELECT serial_id FROM access_groups WHERE name='Admin'))
+	, ('Test', 	'password', (SELECT serial_id FROM access_groups WHERE name='User'))
+	;
+
+INSERT INTO bookings (title, description, start_time, end_time, user_id, resource_id)
+VALUES
+	  (
+		'Event 1'
+		, ''
+		, TIMESTAMP '2024-05-17 10:00:00'
+		, TIMESTAMP '2024-05-17 12:00:00'
+		, (SELECT serial_id FROM users WHERE name='Admin')
+		, (SELECT serial_id FROM resources WHERE name='Alina')
+	  )
+	  , (
+		'Event 2'
+		, ''
+		, TIMESTAMP '2024-05-17 13:00:00'
+		, TIMESTAMP '2024-05-17 16:00:00'
+		, (SELECT serial_id FROM users WHERE name='Test')
+		, (SELECT serial_id FROM resources WHERE name='Groudon')
+	  )
+	;
